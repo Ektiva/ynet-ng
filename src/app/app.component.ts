b70dc3fcd17d68a836b5446dffcbecb24dd024cd
Ektiva <ektiva2013@gmail.com>
2020-05-25 21:58:20 -0400

    Creating and sertup the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,33 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Settings, AppSettings } from './app.settings';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loading: boolean = false;
   public settings: Settings;
-  constructor(public appSettings:AppSettings, public router: Router){
+  products: IProduct[];
+
+  constructor(public appSettings:AppSettings, public router: Router,
+    private http: HttpClient){
     this.settings = this.appSettings.settings;
   }
 
   ngOnInit() {
    // this.router.navigate(['']);  //redirect other pages to homepage on browser refresh    
+   this.http.get('https://localhost:44373/api/products?pageSize=50').subscribe((
+     response: IPagination) => {
+    this.products = response.data;
+   }, error => {
+    console.log(error);
+   });
   }
 
   ngAfterViewInit(){
